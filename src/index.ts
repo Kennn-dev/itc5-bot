a6a17968628bb0c8849ffc8806754f70512bac8a
Ken <nhatong2015@gmail.com>
2023-01-14 01:58:02 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits, Partials } from 'discord.js';
+import './lib/setup';
+
+const client = new SapphireClient({
+	defaultPrefix: 'c5',
+	regexPrefix: /^(hey +)?bot[,! ]/i,
+	caseInsensitiveCommands: true,
+	logger: {
+		level: LogLevel.Debug
+	},
+	shards: 'auto',
+	intents: [
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.MessageContent
+	],
+	partials: [Partials.Channel],
+	loadMessageCommandListeners: true
+});
+
+const main = async () => {
+	try {
+		client.logger.info('Logging in');
+		await client.login();
+		client.logger.info('logged in');
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
+};
+
+main();
